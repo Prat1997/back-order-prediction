d2ca4cac2bbe6c01a60021abdf56864f092f93c3
Sravanthi shoroff <64799973+sravanthishoroff@users.noreply.github.com>
2021-12-15 22:31:35 +0530

    updated main.py

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ logging.basicConfig(filename='info.txt',
 # Load the decision tree model
 model = pickle.load(open('Tuned_DT_Back_order.pkl', 'rb'))
 
+application = app 
 app = Flask(__name__)
 
 
